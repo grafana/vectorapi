17e252b3fd2dccf6ab5aa05217e79effab8bc1bb
Yasir Ekinci <yas.ekinci@grafana.com>
2023-09-06 11:21:18 +0200

    fix embeddings_test

--- tests/routes/embeddings_test.py
+++ tests/routes/embeddings_test.py
@@ -6,8 +6,7 @@ from httpx import AsyncClient
 
 from vectorapi import main
 from vectorapi.embedder import Embedder
-from vectorapi.routes.embeddings import (EmbeddingRequest, EmbeddingResponse,
-                                         SimilarityRequest)
+from vectorapi.routes.embeddings import EmbeddingRequest, EmbeddingResponse, SimilarityRequest
 
 pytestmark = pytest.mark.asyncio
 
@@ -20,7 +19,7 @@ class MockEmbedder(Embedder):
 
 
 # path embedder.encode
-@patch("vectorapi.embeddings.get_embedder")
+@patch("vectorapi.routes.embeddings.get_embedder")
 async def test_embeddings(get_embedder_mock: Mock):
     get_embedder_mock.return_value = MockEmbedder()
     embedding_request = EmbeddingRequest(input="foo")
@@ -34,7 +33,7 @@ async def test_embeddings(get_embedder_mock: Mock):
     assert len(embedding_response.embedding) == 3
 
 
-@patch("vectorapi.embeddings.get_embedder")
+@patch("vectorapi.routes.embeddings.get_embedder")
 async def test_similarity(get_embedder_mock: Mock):
     get_embedder_mock.return_value = MockEmbedder()
     similarity_request = SimilarityRequest(
@@ -48,18 +47,3 @@ async def test_similarity(get_embedder_mock: Mock):
         )
 
     assert response.status_code == 200
-
-
-# async def test_get_models_valid():
-#     app = main.create_app()
-#     async with AsyncClient(app=app, base_url="http://test") as client:
-#         res = await client.get("/models/")
-#         assert len(res.json()["data"]) == 0
-#         assert res.headers["content-type"] == "application/json"
-#         assert res.status_code == 200
-#         await get_mock_model_config_repository().set(
-#             "test_test",
-#             model.ModelConfig(docker_repository="test", version="test", classpath="test"),
-#         )
-#         res = await client.get("/models/")
-#         assert len(res.json()["data"]) == 1
