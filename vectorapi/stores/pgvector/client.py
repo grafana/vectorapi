587f43635a1fb0695906ceae2b08cc95d85f3e13
ioanarm <ioanna.armouti@gmail.com>
2023-09-06 16:24:22 +0300

    enable delete with MetaData

--- vectorapi/stores/pgvector/client.py
+++ vectorapi/stores/pgvector/client.py
@@ -5,14 +5,13 @@ from vectorapi.models.client import Client
 from vectorapi.models.collection import Collection
 from vectorapi.stores.pgvector.client_settings import Settings
 from vectorapi.stores.pgvector.collection import PGVectorCollection
-from sqlalchemy.schema import CreateTable
+from sqlalchemy.schema import CreateTable, DropTable
 
 from sqlalchemy import Table, Column, String, MetaData
 from pgvector.sqlalchemy import Vector
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.sql import text
 from vectorapi.stores.pgvector.db import init_db_engine
-
 from loguru import logger
 
 class PGVectorClient(Client):
@@ -24,12 +23,13 @@ class PGVectorClient(Client):
     async def init_db(self):
         async with self.engine.begin() as conn:
             await conn.execute(text("CREATE EXTENSION IF NOT EXISTS vector"))
-            await conn.commit()
+            # await conn.commit()
 
     async def sync(self):
         pass
 
     async def create_collection(self, name: str, dimension: int) -> Collection:
+        await self.init_db()
         logger.info(f"Creating collection with name {name}")
         collection = Table(
             name,
@@ -66,41 +66,22 @@ class PGVectorClient(Client):
 
     async def delete_collection(self, name: str):
         try:
-            # ## Get PgVectorCollection with name from the self._collections_list
-            # for t in self._collections_list:
-            #     if t.name == name:
-            #         logger.info(f"Found table: {name}")
-            #         drop_expression = DropTable(t._hidden)
-            #         ## Also clean collection from list
-            #         async with self.bound_async_sessionmaker() as session:
-            #             await session.execute(drop_expression)
-            #             await session.commit()
+            table = self.collections.tables.get(name)
+            if table is not None:
+                logger.info(f"Found table: {table}")
+                drop_expression = DropTable(table)
+                logger.info(drop_expression)
 
-            # table = self.collections.tables.get(name)
-            # if table is not None:
-            #     logger.info(f"Found table: {table}")
-            #     drop_expression = DropTable(table)
-            #     logger.info(drop_expression)
+                async with self.bound_async_sessionmaker() as session:
+                    await session.execute(drop_expression)
+                    await session.commit()
+            else:
+                logger.info(f"Table {name} does not exist")
+                raise ValueError(f"Table {name} does not exist")
 
-            #     async with self.bound_async_sessionmaker() as session:
-            #         await session.execute(drop_expression)
-            # else:
-            #     logger.info(f"Table {name} does not exist")
-            #     raise ValueError(f"Table {name} does not exist")
-
-
-            # delete with sql
-            async with self.bound_async_sessionmaker() as session:
-                ## Clean dict of collections
-                # await session.execute(DropTable(self.collections, ifexists=True))
-                await session.execute(text(f"DROP TABLE IF EXISTS {name}"))
-                await session.commit()
-                await session.close()
         except Exception as e:
             logger.exception(e)
             raise e
-
-            # Base.metadata.drop_all(self.engine, [table], checkfirst=True)
 
     async def list_collections(self) -> List[Collection]:
         async with self.engine.begin() as conn:
