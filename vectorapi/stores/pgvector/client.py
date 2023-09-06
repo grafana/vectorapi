f581b57eeed9e665342bf340f2b33b65a278bf9c
ioanarm <ioanna.armouti@gmail.com>
2023-09-06 12:49:34 +0300

    wip: create/delete collection basic functionality

--- vectorapi/stores/pgvector/client.py
+++ vectorapi/stores/pgvector/client.py
@@ -1,5 +1,5 @@
 # import asyncpg
-from typing import List, Optional
+from typing import List, Optional, Dict
 
 from vectorapi.models.client import Client
 from vectorapi.models.collection import Collection
@@ -11,29 +11,29 @@ from sqlalchemy import Table, Column, String, MetaData
 from pgvector.sqlalchemy import Vector
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.sql import text
-from fastapi import Depends
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, AsyncSession
-from sqlalchemy.ext.asyncio import async_sessionmaker
-from vectorapi.stores.pgvector.db import get_session
+from vectorapi.stores.pgvector.db import init_db_engine
+
 from loguru import logger
 
-
 class PGVectorClient(Client):
-    def __init__(self, session: AsyncSession = get_session):
-        self.async_session = session
+    def __init__(self):
+        self.engine, self.bound_async_sessionmaker = init_db_engine()
+        self.collections = MetaData()
+        self._collections_list: List[Dict[str, PGVectorCollection]] = []
 
-        # self.engine = create_async_engine(str(settings.SQLALCHEMY_DATABASE_URL), echo=True, future=True)
-        # self.async_session = async_sessionmaker(
-        #         self.engine,
-        #         expire_on_commit=False,
-        #     )
+    async def init_db(self):
+        async with self.engine.begin() as conn:
+            await conn.execute(text("CREATE EXTENSION IF NOT EXISTS vector"))
+            await conn.commit()
+
+    async def sync(self):
+        pass
 
     async def create_collection(self, name: str, dimension: int) -> Collection:
         logger.info(f"Creating collection with name {name}")
-        meta = MetaData()
         collection = Table(
             name,
-            meta,
+            self.collections,
         Column("id", String, primary_key=True),
         Column("embeddings", Vector(dimension), nullable=False),
         Column(
@@ -46,35 +46,69 @@ class PGVectorClient(Client):
         )
         create_expression = CreateTable(collection)
         logger.info(create_expression)
-
+        col = PGVectorCollection(name=name, dimension=dimension)
+        
         try:
-            await self.async_session().execute(create_expression)
+            async with self.bound_async_sessionmaker() as session:
+                await session.execute(create_expression)
+                await session.commit()
         except Exception as e:
             logger.exception(e)
             raise e
-        return PGVectorCollection(name=name, dimension=dimension, table=collection)
-        
-        #     # async with self.engine.begin() as conn:
-        # async with self.async_session.begin() as session:
-        #     session.scalar()
-            # await conn.run_sync(meta.drop_all)
-            # await conn.run_sync(meta.create_all)
+        return col
 
     
     async def get_collection(self, name: str) -> Optional[Collection]:
-        async with self.engine.begin() as conn:
-            conn.execute(f"SELECT * FROM {name}")
-            pass
+        async with self.bound_async_sessionmaker() as session:
+            await session.execute(text(f"SELECT * FROM {name}"))
+            await session.commit()
+            
 
     async def delete_collection(self, name: str):
-        async with self.engine.begin() as conn:
-            pass
+        try:
+            # ## Get PgVectorCollection with name from the self._collections_list
+            # for t in self._collections_list:
+            #     if t.name == name:
+            #         logger.info(f"Found table: {name}")
+            #         drop_expression = DropTable(t._hidden)
+            #         ## Also clean collection from list
+            #         async with self.bound_async_sessionmaker() as session:
+            #             await session.execute(drop_expression)
+            #             await session.commit()
+
+            # table = self.collections.tables.get(name)
+            # if table is not None:
+            #     logger.info(f"Found table: {table}")
+            #     drop_expression = DropTable(table)
+            #     logger.info(drop_expression)
+
+            #     async with self.bound_async_sessionmaker() as session:
+            #         await session.execute(drop_expression)
+            # else:
+            #     logger.info(f"Table {name} does not exist")
+            #     raise ValueError(f"Table {name} does not exist")
+
+
+            # delete with sql
+            async with self.bound_async_sessionmaker() as session:
+                ## Clean dict of collections
+                # await session.execute(DropTable(self.collections, ifexists=True))
+                await session.execute(text(f"DROP TABLE IF EXISTS {name}"))
+                await session.commit()
+                await session.close()
+        except Exception as e:
+            logger.exception(e)
+            raise e
+
+            # Base.metadata.drop_all(self.engine, [table], checkfirst=True)
 
     async def list_collections(self) -> List[Collection]:
         async with self.engine.begin() as conn:
             pass
 
 
-def get_pgvector_client() -> Client:
+async def get_pgvector_client() -> Client:
     """get_client returns the client instance."""
-    return PGVectorClient(session=AsyncSession)
+    client = PGVectorClient()
+    await client.init_db()
+    return client
--- vectorapi/stores/pgvector/db.py
+++ vectorapi/stores/pgvector/db.py
@@ -8,22 +8,19 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, Asyn
 from vectorapi.stores.pgvector.client_settings import settings
 from typing import AsyncGenerator
 from loguru import logger
+from sqlalchemy.sql import text
 
 
-async_engine = create_async_engine(
-    settings.SQLALCHEMY_DATABASE_URL.unicode_string(),
-    pool_pre_ping=True,
-    echo=settings.ECHO_SQL,
-)
-AsyncSessionLocal = async_sessionmaker(
-    bind=async_engine,
-    autoflush=False,
-    future=True,
-)
-
-async def get_session() -> AsyncGenerator[AsyncSession, None]:
-    async with AsyncSessionLocal() as session:
-        yield session
-
-
-# AsyncSession = Annotated[async_sessionmaker, Depends(get_session)]
+def init_db_engine():
+    async_engine = create_async_engine(
+        settings.SQLALCHEMY_DATABASE_URL.unicode_string(),
+        pool_pre_ping=True,
+        echo=settings.ECHO_SQL,
+    )
+    bound_async_sessionmaker = async_sessionmaker(
+        bind=async_engine,
+        autoflush=False,
+        future=True,
+    )
+    async_engine.connect().execute(text("CREATE EXTENSION IF NOT EXISTS vector"))
+    return (async_engine, bound_async_sessionmaker)
