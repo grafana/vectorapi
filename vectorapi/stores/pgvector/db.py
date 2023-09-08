cddeea0990864e67594a10ecac0b48284423a988
Yasir Ekinci <yas.ekinci@grafana.com>
2023-09-08 15:23:27 +0200

    db: register vector type codec

--- vectorapi/stores/pgvector/db.py
+++ vectorapi/stores/pgvector/db.py
@@ -1,14 +1,7 @@
-import logging
-from typing import Annotated, AsyncIterator
-
-from fastapi import Depends
-from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncSession
-
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from pgvector.utils import from_db_binary, to_db_binary
 from vectorapi.stores.pgvector.client_settings import settings
-from typing import AsyncGenerator
-from loguru import logger
-from sqlalchemy.sql import text
+from sqlalchemy import event, AdaptedConnection
 
 
 def init_db_engine():
@@ -17,10 +10,21 @@ def init_db_engine():
         pool_pre_ping=True,
         echo=settings.ECHO_SQL,
     )
+
+    @event.listens_for(async_engine.sync_engine, "connect")
+    def register_vector(dbapi_connection: AdaptedConnection, *args):
+        stmt = "CREATE EXTENSION IF NOT EXISTS vector"
+        dbapi_connection.run_async(lambda conn: conn.execute(stmt))
+
+        dbapi_connection.run_async(
+            lambda connection: connection.set_type_codec(
+                "vector", encoder=to_db_binary, decoder=from_db_binary, format="binary"
+            )
+        )
+
     bound_async_sessionmaker = async_sessionmaker(
         bind=async_engine,
         autoflush=False,
         future=True,
     )
-    # asyncio.run(async_engine.connect().execute(text("CREATE EXTENSION IF NOT EXISTS vector")))
     return (async_engine, bound_async_sessionmaker)
