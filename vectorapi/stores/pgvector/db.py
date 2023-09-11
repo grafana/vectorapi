ca18721d91483e7cae6100bf51ec57ef422755d2
Yasir Ekinci <yas.ekinci@grafana.com>
2023-09-11 10:29:53 +0200

    get vector dimension from table metadata

--- vectorapi/stores/pgvector/db.py
+++ vectorapi/stores/pgvector/db.py
@@ -4,8 +4,9 @@ from vectorapi.stores.pgvector.client_settings import settings
 from sqlalchemy import event, AdaptedConnection
 
 from typing import Dict, Any
-from sqlalchemy import Table
+from sqlalchemy import Table, text
 from pgvector.sqlalchemy import Vector
+from vectorapi.stores.pgvector.collection import SCHEMA_NAME
 from sqlalchemy.dialects.postgresql.base import PGInspector
 
 
@@ -29,8 +30,31 @@ def init_db_engine():
 
     @event.listens_for(Table, "column_reflect")
     def _setup_vectortype(inspector: PGInspector, table: Table, column_info: Dict[str, Any]):
+        # hacky way to get vector dimensions from the database
         if column_info["name"] == "embedding":
-            column_info["type"] = Vector(768)
+            query = f"""
+SELECT
+	pg_type.typname,
+	pg_attribute.atttypmod
+FROM
+	pg_attribute
+	JOIN pg_class rel ON pg_attribute.attrelid = rel.oid
+	JOIN pg_namespace ON (rel.relnamespace = pg_namespace.oid)
+	JOIN pg_type ON pg_attribute.atttypid = pg_type.oid
+WHERE
+	NOT attisdropped
+	AND pg_attribute.attname = 'embedding'
+	AND rel.relkind = 'r'
+	AND pg_namespace.nspname = '{SCHEMA_NAME}'
+	AND rel.relname = '{table.name}'"""
+            with inspector.engine.begin() as conn:
+                result = conn.execute(text(query)).fetchone()
+                if result is None:
+                    return
+                typname, atttypmod = result
+                if typname != "vector":
+                    return
+                column_info["type"] = Vector(atttypmod)
 
     bound_async_sessionmaker = async_sessionmaker(
         bind=async_engine,
