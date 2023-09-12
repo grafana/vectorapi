09e12393d50bd2f4553cd21c9415c073b7b68839
Yasir Ekinci <yas.ekinci@grafana.com>
2023-09-12 15:22:40 +0200

    Settings: use computed field

--- vectorapi/stores/pgvector/client_settings.py
+++ vectorapi/stores/pgvector/client_settings.py
@@ -1,8 +1,7 @@
 import os
-from pydantic_settings import BaseSettings
-from pydantic import PostgresDsn, validator
 
-from typing import Any
+from pydantic import PostgresDsn, computed_field
+from pydantic_settings import BaseSettings
 
 SCHEMA_NAME = os.getenv("POSTGRES_SCHEMA_NAME", "vector")
 
@@ -15,22 +14,21 @@ class Settings(BaseSettings):
     POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", "0000")
     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "postgres")
     ECHO_SQL: bool = True
-    SQLALCHEMY_DATABASE_URL: PostgresDsn | None = None
 
-    @validator("SQLALCHEMY_DATABASE_URL", pre=True)
-    def assemble_db_connection_string(cls, v: PostgresDsn | None, values: dict[str, Any]) -> Any:
-        if isinstance(v, str):
-            return v
+    @computed_field
+    @property
+    def SQLALCHEMY_DATABASE_URL(self) -> PostgresDsn:
         return PostgresDsn.build(
             scheme="postgresql+asyncpg",
-            username=values["POSTGRES_USER"],
-            password=values["POSTGRES_PASSWORD"],
-            host=values["POSTGRES_HOST"],
-            port=int(values["POSTGRES_PORT"]),
-            path=values['POSTGRES_DB'],
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_HOST,
+            port=int(self.POSTGRES_PORT),
+            path=self.POSTGRES_DB,
         )
-    
+
     class Config:
         case_sensitive = True
+
 
 settings = Settings()
