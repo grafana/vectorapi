1a02fd3a6919549ba3e849b6092ddce9d33d3159
Yasir Ekinci <yas.ekinci@grafana.com>
2023-09-12 16:35:11 +0200

    client_settings: set defaults

--- vectorapi/stores/pgvector/client_settings.py
+++ vectorapi/stores/pgvector/client_settings.py
@@ -9,9 +9,9 @@ SCHEMA_NAME = os.getenv("POSTGRES_SCHEMA_NAME", "vector")
 class Settings(BaseSettings):
     # PostgreSQL Database Connection
     POSTGRES_USER: str = os.getenv("POSTGRES_USER", "postgres")
-    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "postgres")
     POSTGRES_HOST: str = os.getenv("POSTGRES_HOST", "localhost")
-    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", "0000")
+    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", "5432")
     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "postgres")
     ECHO_SQL: bool = True
 
