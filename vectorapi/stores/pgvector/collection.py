edb4d969a0011f4546cf55101226b535fae0b03e
ioanarm <ioanna.armouti@gmail.com>
2023-09-13 16:51:07 +0300

    Add collection not found exception

--- vectorapi/stores/pgvector/collection.py
+++ vectorapi/stores/pgvector/collection.py
@@ -8,9 +8,11 @@ from sqlalchemy import String, delete, select, text
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 from sqlalchemy.ext.declarative import AbstractConcreteBase
-from sqlalchemy.orm import Mapped, declared_attr, mapped_column
-
-from vectorapi.models.collection import Collection, CollectionPoint, CollectionPointResult
+from sqlalchemy.orm import Mapped, mapped_column, declared_attr
+from vectorapi.models.collection import Collection
+from vectorapi.models.collection import CollectionPoint, CollectionPointResult
+from vectorapi.stores.pgvector.exceptions import CollectionPointNotFound
+from sqlalchemy.ext.asyncio import async_sessionmaker
 from vectorapi.stores.pgvector.base import Base
 
 
@@ -147,7 +149,7 @@ class PGVectorCollection(Collection):
         async with self.session_maker() as session:
             result = await self.table.read_by_id(session=session, point_id=id)
             if result is None:
-                raise Exception(f"Point with id {id} does not exist")
+                raise CollectionPointNotFound(f"Collection point with id {id} not found in collection {self.name}")
             return CollectionPoint(
                 id=result.id, embedding=result.embedding, metadata=result.metadatas
             )
--- vectorapi/stores/pgvector/exceptions.py
+++ vectorapi/stores/pgvector/exceptions.py
@@ -1,10 +1,19 @@
 __all__ = [
     "CollectionNotFound",
+    "CollectionPointNotFound",
 ]
 
 class CollectionNotFound(Exception):
     """
     Exception raised when attempting to get a collection that does not exist
+    """
+
+    ...
+
+
+class CollectionPointNotFound(Exception):
+    """
+    Exception raised when attempting to get a collection point that does not exist
     """
 
     ...
