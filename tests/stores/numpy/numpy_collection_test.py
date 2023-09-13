322feab1188957ab0986743d24ce964a83c353fa
ioanarm <ioanna.armouti@gmail.com>
2023-09-13 19:01:48 +0300

    Add unit tests for numpy client

---
+++ tests/stores/numpy/numpy_collection_test.py
@@ -0,0 +1,61 @@
+import pytest
+
+from vectorapi.stores.numpy.client import NumpyClient
+from vectorapi.models.collection import CollectionPoint
+
+
+class TestNumpyCollection:
+    @pytest.mark.asyncio
+    async def test_insert(self):
+        client = NumpyClient()
+        collection = await client.create_collection("test_collection", 2)
+        await collection.insert("1", [1.0, 2.0], {})
+        point = await collection.get("1")
+        assert isinstance(point, CollectionPoint)
+        assert point.id == "1"
+        assert point.embedding == [1.0, 2.0]
+        assert point.metadata == {}
+
+    @pytest.mark.asyncio
+    async def test_update(self):
+        client = NumpyClient()
+        collection = await client.create_collection("test_collection", 2)
+        await collection.insert("1", [1.0, 2.0], {})
+        await collection.update("1", [2.0, 3.0], {})
+        point = await collection.get("1")
+        assert point.id == "1"
+        assert point.embedding == [2.0, 3.0]
+        assert point.metadata == {}
+
+    @pytest.mark.asyncio
+    async def test_delete(self):
+        client = NumpyClient()
+        collection = await client.create_collection("test_collection", 2)
+        await collection.insert("1", [1.0, 2.0], {})
+        await collection.delete("1")
+        # TODO: Add exceptions
+        with pytest.raises(ValueError):
+            assert await collection.get("1")
+
+    @pytest.mark.asyncio
+    async def test_query(self):
+        client = NumpyClient()
+        collection = await client.create_collection("test_collection", 2)
+        await collection.insert("1", [1.0, 2.0], {})
+        await collection.insert("2", [2.0, 4.0], {})
+        await collection.insert("3", [3.0, 5.0], {})
+        
+        #TODO: cosine similarity seems weird here. We need to spend some time to understand this.
+        results = await collection.query([2.0, 4.0], limit=3)
+        assert len(results) == 3
+
+    @pytest.mark.asyncio
+    async def test_upsert(self):
+        client = NumpyClient()
+        collection = await client.create_collection("test_collection", 2)
+        await collection.upsert("1", [1.0, 2.0], {})
+        await collection.upsert("1", [2.0, 3.0], {})
+        point = await collection.get("1")
+        assert point.id == "1"
+        assert point.embedding == [2.0, 3.0]
+        assert point.metadata == {}
