5fa38212c6fd7a29f740b27a4edc96291326fd2d
Yasir Ekinci <yas.ekinci@grafana.com>
2023-10-18 22:04:50 +0200

    log: update intercept emit logic

--- vectorapi/log.py
+++ vectorapi/log.py
@@ -29,17 +29,19 @@ class InterceptHandler(logging.Handler):
     See https://loguru.readthedocs.io/en/stable/overview.html
     """
 
-    def emit(self, record: logging.LogRecord):
-        level = _get_log_level(record)
+    def emit(self, record: logging.LogRecord) -> None:
+        # Get corresponding Loguru level if it exists.
+        level: str | int
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
 
-        # Find caller from where originated the logged message
-        frame, depth = logging.currentframe(), 2
-        while frame.f_code.co_filename == logging.__file__:
-            if frame.f_back is not None:
-                frame = frame.f_back
-                depth += 1
-            else:
-                break
+        # Find caller from where originated the logged message.
+        frame, depth = inspect.currentframe(), 0
+        while frame and (depth == 0 or frame.f_code.co_filename == logging.__file__):
+            frame = frame.f_back
+            depth += 1
 
         logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
 
